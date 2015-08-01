2a07ed5e0b0360de3b162fb03702e2d5e71d12dd
Phil <marshall.phillip@gmail.com>
2015-08-01 10:56:43 -0400

    js

--- myPlayer.js
+++ myPlayer.js
@@ -1,2 +1,18 @@
-<script src=http://pmarsh41.github.io/jwplayer/jwplayer.js"></script>
-<script>jwplayer.key="iZaBV7cW7mBl61yIWCTK3IUeA1jqZjIUo28r0w==";</script>
+< script src = "http://pmarsh41.github.io/jwplayer/jwplayer.js" > < /script> < script > jwplayer.key = "iZaBV7cW7mBl61yIWCTK3IUeA1jqZjIUo28r0w=="; < /script >
+
+;
+(function(exports) {
+    ///Bookmark, Line 74///
+    "use strict";
+    //Constructor Code
+
+    var playerInstance = jwplayer('container');
+    //jwplayer('container').pause(); // BAD! :(
+    playerInstance.pause(); // Good :)
+}),
+var playerInstance = jwplayer('container');
+
+playerInstance.on('complete', function(e) {
+    setElement('complete', "Thanks for Watching");
+
+});
